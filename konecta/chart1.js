67a86735a14e0a105e5e39faeb5f4fba312b32cc
andreaSanchezC <111660649+andreaSanchezC@users.noreply.github.com>
2022-10-29 12:04:06 -0500

    Add files via upload

---
+++ konecta/chart1.js
@@ -0,0 +1,39 @@
+var ctx = document.getElementById('barChart').getContext('2d');
+var myChart = new Chart(ctx, {
+
+    type: 'bar',
+    data: {
+        labels: ['María', 'Jenny', 'Juan'],
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: [9, 12, 5],
+                borderColor: 'rgb(255, 153, 51)',
+                backgroundColor: 'rgb(255, 153, 51)',
+                order: 1
+            },
+            
+            {
+                label: 'Dataset 4',
+                data: [9, 12, 5],
+                borderColor: 'rgb(255, 102, 102)',
+                backgroundColor: 'rgb(255, 102, 102)',
+                type: 'line',
+                order: 0
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+              position: 'top',
+              display: false,
+            },
+            title: {
+              display: false,
+            }
+    }
+}
+});
+
---
+++ konecta/chart2.js
@@ -0,0 +1,59 @@
+var ctx2 = document.getElementById('doughnut').getContext('2d');
+var myChart2 = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: ['Ventas', 'No ventas', 'Ventas pospuestas'],
+
+        datasets: [{
+            label: 'Dataset 1',
+            data: [9, 12, 5],
+            backgroundColor: ['rgb(255, 102, 102)', 'rgb(255, 153, 51)','rgb(255, 226, 75)'],
+            borderColor: ['rgb(255, 102, 102)', 'rgb(255, 153, 51)','rgb(255, 226, 75)'],
+            borderWidth: 1
+        }]
+
+    },
+    options: {
+        responsive: true
+        
+    }
+});
+/*
+<script>
+        const data = {
+            labels: ['Maquina', 'Fileteadora', 'Cortadora', 'Pulidora', 'Nevera industrial'],
+            datasets: [
+                {
+                    label: 'Dataset 1',
+                    data: [9, 12, 5, 10, 7],
+                    backgroundColor: ['rgb(0, 204, 204)', 'rgb(51, 153, 255)','rgb(255, 102, 102)', 'rgb(255, 153, 51)','rgb(255, 226, 75)'],
+                }
+            ]
+        };
+        // </block:setup>
+
+        // <block:config:0>
+        const config = {
+            type: 'doughnut',
+            data: data,
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Rendimiento de productos'
+                    }
+                }
+            },
+        };
+    </script>
+    <script>
+        const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+    </script>
+*/
