92a7cfe5f856a64cb1ae5bc2ae16a7b835ee6fba
kevinsotoguerrero <kssotog@unal.edu.co>
2022-10-29 12:45:17 -0500

    feat: agentes

---
+++ konecta/tabla.js
@@ -0,0 +1,16 @@
+new gridjs.Grid({
+    columns: ["Agente", "Total llamadas", "Ventas", "No ventas", "Reprogramadas", "Estado"],
+    data: [
+      ["John", "2", "1", "1", "0", "Disponible"],
+      ["John", "2", "1", "1", "0", "Disponible"],
+      ["John", "2", "1", "1", "0", "Disponible"],
+      ["John", "2", "1", "1", "0", "Disponible"],
+      ["John", "2", "1", "1", "0", "Disponible"],
+      ["John", "2", "1", "1", "0", "Disponible"],
+      ["John", "2", "1", "1", "0", "Disponible"],
+    ]
+  }).render(document.getElementById("wrapper"));
+
+
+
+
